dac9b5f3cf711ec507cb294fc2dc2c711d627eff
Humberto Tejada <humtej1204@gmail.com>
2021-12-21 12:42:31 -0500

    creating files and adding scripts for task 00 and 01

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+        n1 = -number
+else:
+        n1 = number
+
+n = n1 % 10
+
+if n > 5:
+        str = "and is greater than 5"
+elif n == 0:
+        str = "and is 0"
+else:
+        str = "and is less than 6 and not 0"
+print("Last digit of {:d} is {:d} {:s}".format(number,n,str))

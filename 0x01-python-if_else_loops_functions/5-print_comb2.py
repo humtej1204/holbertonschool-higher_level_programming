e1d25e4c46adcd1aefc3234631b562dde768e830
Humberto Tejada <humtej1204@gmail.com>
2021-12-21 22:33:38 -0500

    correcting script

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 number = 0
-while number < 99:
-    print('{} = {}'.format(number, hex(number)))
+while number <= 99:
+    if number != 99:
+        print("{:02d}, ".format(number), end='')
+    else:
+        print("{:02d}".format(number))
     number = number + 1

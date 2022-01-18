3453377bb8e9e60282c9728157f85c3b181d58ab
Humberto Tejada <humtej1204@gmail.com>
2022-01-17 22:25:06 -0500

    correcting trailing whitespace in the line 10

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,7 +7,7 @@ def list_division(my_list_1, my_list_2, list_length):
             c = my_list_1[a] / my_list_2[a]
         except TypeError:
             c = 0
-            print("wrong type") 
+            print("wrong type")
         except IndexError:
             c = 0
             print("out of range")

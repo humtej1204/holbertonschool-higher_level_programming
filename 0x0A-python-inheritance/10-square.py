25fc291c71ebe72fbb1d5ebe222b6f81710131f3
Humberto Tejada <humtej1204@gmail.com>
2022-01-31 23:15:23 -0500

    Correcting code

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -3,33 +3,11 @@
 '''Task 10 - 10. Square #1'''
 
 
-class BaseGeometry():
-    '''class BaseGeometry'''
-
-    def area(self):
-        '''Function that raises an Exception with the message area()
-        is not implemented'''
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        '''that validates value:
-        if value is not an integer: raise a TypeError exception
-        if value is less or equal to 0:
-        raise a ValueError exception with the message'''
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Class Square that inherits from Rectangle (9-rectangle.py):"""
     def __init__(self, size):
-        """Instantiation with size"""
-        self.integer_validator("size", size)
-        self.__size = size
+        '''Initializing atributes'''
         super().__init__(size, size)
-
-    def area(self):
-        """Return the area of the square"""
-        return self.__size ** 2
+        super().integer_validator("size", size)
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,12 +5,13 @@
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     '''class Rectangle'''
 
     def __init__(self, width, height):
         '''Initializing atributes'''
-        BaseGeometry.integer_validator(self, "width", width)
+        super().integer_validator("width", width)
         self.__width = width
-        BaseGeometry.integer_validator(self, "height", height)
+        super().integer_validator("height", height)
         self.__height = height

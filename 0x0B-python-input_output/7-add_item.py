b839ee06e956dda224525760a83f21d37628f9c6
Humberto Tejada Espinoza <90359858+humtej1204@users.noreply.github.com>
2022-02-01 16:55:01 -0500

    Correcting code

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,8 +6,8 @@
 import sys
 
 
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
     loadFile = load_from_json_file("add_item.json")

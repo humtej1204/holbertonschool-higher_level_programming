5c5d8eda5729318be83431d08b054caf264049e4
Humberto Tejada Espinoza <90359858+humtej1204@users.noreply.github.com>
2022-01-31 23:41:08 -0500

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -10,6 +10,6 @@ class Square(Rectangle):
     '''class Square'''
     def __init__(self, size):
         '''Initializing atributes'''
+        self.integer_validator("size", size)
         self.__size = size
-        super().__init__(size, size)
-        super().integer_validator("size", size)
+        super().__init__(self.__size, self.__size)

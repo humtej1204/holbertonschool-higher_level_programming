ca351ab8b2a447244a2e4410da5dd9d7d78749f5
Humberto Tejada <humtej1204@gmail.com>
2022-01-31 23:07:39 -0500

    Correcting code

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -26,7 +26,7 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         '''Initializing atributes'''
-        super().integer_validator("width", width)
+        BaseGeometry.integer_validator(self, "width", width)
         self.__width = width
-        super().integer_validator("height", height)
+        BaseGeometry.integer_validator(self, "height", height)
         self.__height = height

6f0c735d518588c442383b8b8a4798803bc45934
Humberto Tejada <humtej1204@gmail.com>
2022-01-31 23:17:10 -0500

    Correcting code

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,6 +7,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    '''class Square'''
     def __init__(self, size):
         '''Initializing atributes'''
         super().__init__(size, size)

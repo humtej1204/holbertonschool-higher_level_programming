93f9ab1239e7d7fec114e1b5bb36a1d81fc08f2a
Humberto Tejada Espinoza <90359858+humtej1204@users.noreply.github.com>
2022-02-01 21:16:01 -0500

    Correcting except

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,7 +11,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
     f = load_from_json_file("add_item.json")
-except FileNotFoundError:
+except:
     f = []
 
 argc = len(sys.argv)

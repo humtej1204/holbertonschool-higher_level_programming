c8fc2e8cd5a53feb90e26b63537308b3e62704e3
Humberto Tejada Espinoza <90359858+humtej1204@users.noreply.github.com>
2022-02-07 12:44:46 -0500

    Line 39: Correcting Conditional 0.2

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -36,7 +36,7 @@ class Base():
 
     @staticmethod
     def from_json_string(json_string):
-        if json_string is None or json_string == "\0":
+        if json_string is None or json_string == "":
             return "[]"
         else:
             return json.loads(json_string)

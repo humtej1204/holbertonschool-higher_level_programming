b54a1449799d0c0741c5f4429464dd8e6ad3acaf
Humberto Tejada Espinoza <90359858+humtej1204@users.noreply.github.com>
2022-02-07 12:43:54 -0500

    Line 39: Correct conditional

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -36,7 +36,7 @@ class Base():
 
     @staticmethod
     def from_json_string(json_string):
-        if json_string is None or len(json_string) == 0:
+        if json_string is None or json_string == "\0":
             return "[]"
         else:
             return json.loads(json_string)

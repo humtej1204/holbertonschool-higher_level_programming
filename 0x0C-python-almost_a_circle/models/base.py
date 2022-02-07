4931cc87967422f7c5509169694b58814395e4c0
Humberto Tejada Espinoza <90359858+humtej1204@users.noreply.github.com>
2022-02-07 12:46:35 -0500

    Correcting staticmethod from_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -37,7 +37,8 @@ class Base():
     @staticmethod
     def from_json_string(json_string):
         if json_string is None or json_string == "":
-            return "[]"
+            ls = []
+            return ls
         else:
             return json.loads(json_string)
 

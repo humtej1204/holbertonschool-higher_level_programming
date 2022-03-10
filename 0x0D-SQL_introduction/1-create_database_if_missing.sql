2a3bbeeee1457701a5c8711f82ac9f65a89ba306
Humberto Tejada <humtej1204@gmail.com>
2022-03-10 12:02:07 -0500

    Creating files and writting the code

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- Script that creates the database hbtn_0c_0 in your MySQL server
+CREATE DATABASE hbtn_0c_0 IF NOT EXIST;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- Script that deletes the database hbtn_0c_0 in your MySQL server
+DROP DATABASE hbtn_0c_0 IF EXIST;

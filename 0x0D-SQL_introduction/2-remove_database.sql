31e66381c11742452cce32bf0cfe9eefc1809df0
Humberto Tejada <humtej1204@gmail.com>
2022-03-10 12:06:22 -0500

    Correcting conditional position

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
 -- Script that deletes the database hbtn_0c_0 in your MySQL server
-DROP DATABASE hbtn_0c_0 IF EXIST;
+DROP DATABASE IF EXIST hbtn_0c_0;
--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- Script that creates a table called first_table in the current database in your MySQL server
-CREATE TABLE first_table (id INT, name VARCHAR(256)) IF NOT EXIST;
+CREATE TABLE IF NOT EXIST first_table (id INT, name VARCHAR(256));

f8c51d0cc58cffb2616cbe43964819985f1872b7
Humberto Tejada <humtej1204@gmail.com>
2022-05-18 07:59:20 -0500

    Correcting code. Change All

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,15 +1,16 @@
 #!/usr/bin/node
-const axios = require('axios').default;
-const id = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+const axios = require('axios');
+const id = process.argv.slice(process.argv.length - 1);
+const url = `https://swapi-api.hbtn.io/api/films/${id}/`;
 
-axios.get(url)
-  .then(({ data }) => {
-    /* console.log(data.characters); */
-    data.characters.forEach((char) => {
-      axios.get(char)
-        .then(({ data }) => console.log(data.name))
-        .catch(err => console.log(err.message));
+axios.get(url).then(res => {
+  const dat = res.data.characters;
+
+  dat.forEach(x => {
+    axios.get(x).then(d => {
+      const dat = d.data.name;
+      console.log(dat);
     });
-  })
-  .catch(err => console.log(err.message));
+  });
+  /* console.log(res.data); */
+});
